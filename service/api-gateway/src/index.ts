ebed80619f75832ea55936fd197cf8d35c382d5b
qyy-lbs <husnihaqiqi3@gmail.com>
2026-04-25 19:25:14 +0700

    api-gateway

--- service/api-gateway/src/index.ts
+++ service/api-gateway/src/index.ts
@@ -3,13 +3,16 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { createProxyMiddleware, fixRequestBody } from 'http-proxy-middleware';
 
+// 1. IMPORT SATPAM KITA DI SINI
+import { verifyGatewayToken } from './middlewares/auth.middleware';
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-// --- PROXY AUTH SERVICE ---
+// --- PROXY AUTH SERVICE (PINTU PUBLIK - TANPA SATPAM) ---
 app.use(createProxyMiddleware({
   pathFilter: '/api/auth', // Menangkap semua yang berawal /api/auth
   target: process.env.AUTH_SERVICE_URL || 'http://localhost:3002',
@@ -22,16 +25,25 @@ app.use(createProxyMiddleware({
   }
 }));
 
-// --- PROXY INBOUND SERVICE ---
-app.use(createProxyMiddleware({
-  pathFilter: '/api/inbound',
-  target: process.env.INBOUND_SERVICE_URL || 'http://localhost:3003',
-  changeOrigin: true,
-  on: {
-    proxyReq: fixRequestBody,
-  }
-}));
+// --- PROXY INBOUND SERVICE (PINTU TERKUNCI - PAKAI SATPAM) ---
+// 2. KITA PASANG SATPAM DI RUTE INI
+app.use(
+  '/api/inbound', 
+  verifyGatewayToken, // <--- Satpam mencegat di sini sebelum diteruskan ke Inbound
+  createProxyMiddleware({
+    // pathFilter tidak perlu lagi karena sudah ditangkap oleh app.use('/api/inbound')
+    target: process.env.INBOUND_SERVICE_URL || 'http://localhost:3003',
+    changeOrigin: true,
+    on: {
+      proxyReq: fixRequestBody,
+      proxyRes: (proxyRes, req) => {
+        console.log(`[Inbound-Service] ${req.method} ${req.url} -> Status: ${proxyRes.statusCode}`);
+      }
+    }
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`🚀 Gateway UIKA Berhasil di Port ${PORT}`);
+  console.log(`🛡️  Middleware Keamanan: AKTIF`);
 });
