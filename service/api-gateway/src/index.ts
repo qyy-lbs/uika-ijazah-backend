1fb72d17128021b7103bd91f44bea44ed182503f
qyy-lbs <husnihaqiqi3@gmail.com>
2026-04-30 19:00:12 +0700

    perbarui

--- service/api-gateway/src/index.ts
+++ service/api-gateway/src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors()); //mengizinkan semua domain untuk binding
 
 // --- PROXY AUTH SERVICE (PINTU PUBLIK - TANPA SATPAM) ---
 app.use(createProxyMiddleware({
