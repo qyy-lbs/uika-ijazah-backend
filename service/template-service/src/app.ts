3f597f507a7b288b50bee33f2ce785cec3e05a3c
qyy-lbs <husnihaqiqi3@gmail.com>
2026-06-05 22:40:15 +0700

    update : module template

--- service/template-service/src/app.ts
+++ service/template-service/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import path from "path";
 import templateRoutes from "./routes/template.routes.js";
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
