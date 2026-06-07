00153dc9b5314eabf1e9ecc5ee8620ced1fbf7e9
qyy-lbs <husnihaqiqi3@gmail.com>
2026-06-08 02:28:12 +0700

    Remove blockchain service from development

--- service/blockchain-service/src/server.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import app from "./app.js";
-
-const PORT = process.env.PORT || 3011;
-
-app.listen(PORT, () => {
-  console.log(`Blockchain Service berjalan di port ${PORT}`);
-});
